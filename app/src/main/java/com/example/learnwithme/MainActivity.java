d180693641b5ce67efdd8895f78c09bb885e47d3
UzairAsadBaig <uzairasad26@gmail.com>
2022-06-06 20:00:01 +0500

    Update MainActivity.java

--- app/src/main/java/com/example/learnwithme/MainActivity.java
+++ app/src/main/java/com/example/learnwithme/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.learnwithme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button githubRep;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        githubRep = findViewById(R.id.githubRep);
+
     }
 }
