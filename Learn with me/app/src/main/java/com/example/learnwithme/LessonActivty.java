f5c58591a1485c353275533b922ea543c6cf42f8
UzairAsadBaig <uzairasad26@gmail.com>
2022-06-07 19:36:19 +0500

    Update LessonActivty.java

--- Learn with me/app/src/main/java/com/example/learnwithme/LessonActivty.java
+++ Learn with me/app/src/main/java/com/example/learnwithme/LessonActivty.java
@@ -2,13 +2,23 @@ package com.example.learnwithme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LessonActivty extends AppCompatActivity {
-
+    Button a;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson_activty);
+        a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LessonActivty.this,singleLesson.class);
+                startActivity(intent);
+            }
+        });
     }
 }
