e560c88a900d4e40809cc4784504357322798ca6
UzairAsadBaig <uzairasad26@gmail.com>
2022-06-08 20:21:03 +0500

    Update LessonActivty.java

--- Learn with me/app/src/main/java/com/example/learnwithme/LessonActivty.java
+++ Learn with me/app/src/main/java/com/example/learnwithme/LessonActivty.java
@@ -199,5 +199,15 @@ public class LessonActivty extends AppCompatActivity {
         });
 
 
+        findViewById(R.id.u).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LessonActivty.this,singleLesson.class);
+                intent.putExtra("name","u");
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
