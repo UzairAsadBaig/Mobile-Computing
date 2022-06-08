a62215add7ff797265f2f298157b1a3cbfd52ace
UzairAsadBaig <uzairasad26@gmail.com>
2022-06-08 20:18:14 +0500

    Update LessonActivty.java

--- Learn with me/app/src/main/java/com/example/learnwithme/LessonActivty.java
+++ Learn with me/app/src/main/java/com/example/learnwithme/LessonActivty.java
@@ -140,6 +140,15 @@ public class LessonActivty extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.o).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LessonActivty.this,singleLesson.class);
+                intent.putExtra("name","o");
+                startActivity(intent);
+            }
+        });
+
 
     }
 }
