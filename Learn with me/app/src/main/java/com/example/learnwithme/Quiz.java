470f81ef3809c9bc0d7580410afc3241ce835d14
UzairAsadBaig <uzairasad26@gmail.com>
2022-06-09 19:03:21 +0500

    Added quiz

---
+++ Learn with me/app/src/main/java/com/example/learnwithme/Quiz.java
@@ -0,0 +1,14 @@
+package com.example.learnwithme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Quiz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
+}
