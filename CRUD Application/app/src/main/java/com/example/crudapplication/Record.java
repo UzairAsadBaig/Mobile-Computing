8110a758f51b5eb8553038388f5180629e913247
UzairAsadBaig <uzairasad26@gmail.com>
2022-08-20 18:21:12 +0500

    Displayed data on record page

--- CRUD Application/app/src/main/java/com/example/crudapplication/Record.java
+++ CRUD Application/app/src/main/java/com/example/crudapplication/Record.java
@@ -4,19 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Record extends AppCompatActivity {
+
+    EditText name,rollNo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
 
+        name = findViewById(R.id.name);
+        rollNo = findViewById(R.id.rollNo);
+
         Intent intent = getIntent();
 
         String stName = intent.getStringExtra("name");
         Boolean isEnroll = intent.getBooleanExtra("isEnroll",false);
-        int roll = intent.getIntExtra("roll",-1);
+        int stRoll = intent.getIntExtra("roll",-1);
+
+        name.setText(stName);
+        rollNo.setText(stRoll+"");
+
 
 
 
