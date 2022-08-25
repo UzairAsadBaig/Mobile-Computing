a4ac5c3c8dfffc300851e16b0df33faaea405d47
UzairAsadBaig <uzairasad26@gmail.com>
2022-08-26 03:18:41 +0500

    Update SurahModel.java

--- AlHuda/app/src/main/java/com/example/alhuda/SurahModel.java
+++ AlHuda/app/src/main/java/com/example/alhuda/SurahModel.java
@@ -1,4 +1,36 @@
 package com.example.alhuda;
 
+
 public class SurahModel {
+    String name;
+    int id;
+
+    @Override
+    public String toString() {
+        return id +" "+ name;
+    }
+
+    public SurahModel(String name) {
+        this.name = name;
+    }
+    public SurahModel(String name, int id) {
+        this.name = name;
+        this.id= id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
