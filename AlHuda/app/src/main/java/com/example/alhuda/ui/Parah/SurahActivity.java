4faea13990777ad0edb9af74a721e775a73d593c
UzairAsadBaig <uzairasad26@gmail.com>
2022-08-26 04:10:41 +0500

    Added Surah Activity

---
+++ AlHuda/app/src/main/java/com/example/alhuda/ui/Parah/SurahActivity.java
@@ -0,0 +1,14 @@
+package com.example.alhuda.ui.Parah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SurahActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_surah);
+    }
+}
