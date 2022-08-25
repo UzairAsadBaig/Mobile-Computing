41eb6767ca701d984d4b15979e2c62d319b86b0c
UzairAsadBaig <uzairasad26@gmail.com>
2022-08-26 03:14:31 +0500

    Update DBHelper.java

--- AlHuda/app/src/main/java/com/example/alhuda/DBHelper.java
+++ AlHuda/app/src/main/java/com/example/alhuda/DBHelper.java
@@ -1,4 +1,35 @@
 package com.example.alhuda;
 
-public class DBHelper {
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import com.example.quranapp.ui.gallery.GalleryFragment;
+
+import java.util.ArrayList;
+
+public class DbHelper extends SQLiteOpenHelper{
+
+
+
+    public DbHelper(@Nullable Context context) {
+        super(context, "quran_database_new.db", null, 1);
+    }
+
+
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
 }
