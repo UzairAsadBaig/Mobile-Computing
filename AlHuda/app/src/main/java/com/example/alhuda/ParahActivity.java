72cea6c1d1b43b4ecaeced0cd59cb269fd4eafaf
UzairAsadBaig <uzairasad26@gmail.com>
2022-08-27 04:18:30 +0500

    Update ParahActivity.java

--- AlHuda/app/src/main/java/com/example/alhuda/ParahActivity.java
+++ AlHuda/app/src/main/java/com/example/alhuda/ParahActivity.java
@@ -2,7 +2,12 @@ package com.example.alhuda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class ParahActivity extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class ParahActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parah);
+        Intent i = getIntent();
+        String id=i.getStringExtra("id");
+        setTitle("Parah "+id);
+        ListView listView = findViewById(R.id.parahListView);
+        DbHelper dbHelper = new DbHelper(ParahActivity.this);
+        List<SurahModel> list = dbHelper.getParaDetails(Integer.parseInt(id));
+        ArrayAdapter ad =new ArrayAdapter(ParahActivity.this, android.R.layout.simple_list_item_1,list);
+        listView.setAdapter(ad);
     }
 }
