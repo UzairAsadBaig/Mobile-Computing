ab67698dcbbeb6523ec6a697627f15007a120823
UzairAsadBaig <uzairasad26@gmail.com>
2022-08-27 04:15:37 +0500

    Added Parah Activity

---
+++ AlHuda/app/src/main/java/com/example/alhuda/ParahActivity.java
@@ -0,0 +1,14 @@
+package com.example.alhuda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ParahActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parah);
+    }
+}
