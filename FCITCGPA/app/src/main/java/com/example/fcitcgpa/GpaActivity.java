eb77c13b3479d7bf84b8c609ddf55e2085668c78
UzairAsadBaig <uzairasad26@gmail.com>
2022-08-27 18:49:20 -0400

    Completed the GPA and CGPA input interface design

--- FCITCGPA/app/src/main/java/com/example/fcitcgpa/GpaActivity.java
+++ FCITCGPA/app/src/main/java/com/example/fcitcgpa/GpaActivity.java
@@ -1,5 +1,4 @@
 package com.example.fcitcgpa;
-
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,20 +6,19 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
-
-public class CgpaActivity extends AppCompatActivity {
-   Spinner gp1,gp2,gp3,gp4,gp5,gp6;
+public class GpaActivity extends AppCompatActivity {
+    Spinner gp1,gp2,gp3,gp4,gp5,gp6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cgpa);
+        setContentView(R.layout.activity_gpa);
         gp1 = findViewById(R.id.gp1);
         gp2 = findViewById(R.id.gp2);
         gp3 = findViewById(R.id.gp3);
         gp4 = findViewById(R.id.gp4);
         gp5 = findViewById(R.id.gp5);
         gp6 = findViewById(R.id.gp6);
-        ArrayAdapter<CharSequence>adapter=ArrayAdapter.createFromResource(this, R.array.grades, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.grades, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
 
         gp1.setAdapter(adapter);
--- FCITCGPA/app/src/main/java/com/example/fcitcgpa/MainActivity.java
+++ FCITCGPA/app/src/main/java/com/example/fcitcgpa/MainActivity.java
@@ -8,16 +8,24 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button CgpaBtn;
+    Button CgpaBtn,GpaBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         CgpaBtn = (Button) findViewById(R.id.calCgpa);
+        GpaBtn = (Button) findViewById(R.id.calGpa);
         CgpaBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this,CgpaActivity.class);
+                startActivity(intent);
+            }
+        });
+        GpaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,GpaActivity.class);
                 startActivity(intent);
             }
         });
