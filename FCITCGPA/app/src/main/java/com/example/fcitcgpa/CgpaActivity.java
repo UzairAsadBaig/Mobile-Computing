2133186a052d51939789cb6b83b9c884cabada3e
UzairAsadBaig <uzairasad26@gmail.com>
2022-08-27 14:12:38 -0400

    Added a new CgpaActivty and linked with MainActivity

---
+++ FCITCGPA/app/src/main/java/com/example/fcitcgpa/CgpaActivity.java
@@ -0,0 +1,14 @@
+package com.example.fcitcgpa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CgpaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cgpa);
+    }
+}
--- FCITCGPA/app/src/main/java/com/example/fcitcgpa/MainActivity.java
+++ FCITCGPA/app/src/main/java/com/example/fcitcgpa/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.fcitcgpa;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button CgpaBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CgpaBtn = (Button) findViewById(R.id.calCgpa);
+        CgpaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,CgpaActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
