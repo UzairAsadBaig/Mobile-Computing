b9b7a3e6048138b3310c1cf44c820b572c642cf8
UzairAsadBaig <uzairasad26@gmail.com>
2022-08-27 15:23:48 -0400

    Prepared layout for CGPA page

--- FCITCGPA/app/src/main/java/com/example/fcitcgpa/CgpaActivity.java
+++ FCITCGPA/app/src/main/java/com/example/fcitcgpa/CgpaActivity.java
@@ -1,14 +1,23 @@
 package com.example.fcitcgpa;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class CgpaActivity extends AppCompatActivity {
-
+   Spinner gp1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cgpa);
+        gp1 = findViewById(R.id.gp1);
+        ArrayAdapter<CharSequence>adapter=ArrayAdapter.createFromResource(this, R.array.grades, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+
+        gp1.setAdapter(adapter);
     }
 }
