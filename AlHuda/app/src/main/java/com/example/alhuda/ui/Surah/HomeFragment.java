53ed7811593c1a4df3fd0c077195e2245ef70e04
UzairAsadBaig <uzairasad26@gmail.com>
2022-08-28 17:01:16 +0500

    Design Modification

--- AlHuda/app/src/main/java/com/example/alhuda/ui/Surah/HomeFragment.java
+++ AlHuda/app/src/main/java/com/example/alhuda/ui/Surah/HomeFragment.java
@@ -29,7 +29,6 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View contentView = inflater.inflate(R.layout.fragment_home, container, false);
-
         ListView listView = contentView.findViewById(R.id.listview);
         DbHelper dbHelper = new DbHelper(getContext());
         List<SurahModel> list = dbHelper.getAllSurahNames();
